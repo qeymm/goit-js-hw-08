9e4cc827cb333f4e5bf82f3169412c1feff713e3
Kim Enorme <kimenorme15@gmail.com>
2025-05-11 16:30:31 +0800

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form.feedback-form');
 const emailEl = document.querySelector('label [name="email"]');
@@ -6,41 +6,46 @@ const messageEl = document.querySelector('label [name="message"]');
 
 const STORAGE_KEY = 'feedback-form-state';
 
-function onPageReload() {
-    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (savedMessage) {
-        emailEl.value = savedMessage;
-        messageEl.value = savedMessage;
-    }
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const email = emailEl.value;
+  const message = messageEl.value;
+
+  const formData = {
+    email,
+    message,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 onPageReload();
 
-function onFormInput() {
-    const email = emailEl.value;
-    const message = messageEl.value;
-
-    const formData = {
-        email,
-        message,
-    };
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-form.addEventListener('input', throttle(onFormInput, 500));
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    const email = emailEl.value;
-    const message = messageEl.value;
-
-    if (email === '' || message === '') {
-        alert('Please fill in all fields!');
-        form.reset();
-        return;
-    } 
-       form.reset();
-    localStorage.removeItem(STORAGE_KEY);
+function onPageReload() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    emailEl.value = savedMessage.email;
+    messageEl.value = savedMessage.message;
+  }
 }
 
-form.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = emailEl.value;
+  const message = messageEl.value;
+
+  if (email == '' || message == '') {
+    alert('Enter both input parameters!');
+    form.reset();
+    return;
+  }
+
+  const formData = { email, message };
+  console.log(formData);
+  form.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
